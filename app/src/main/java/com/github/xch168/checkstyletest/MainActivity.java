acfeb8853aab7ce0d5cbc1c0d6d43e40b6bad89b
辉天神龙 <122565648@qq.com>
2018-12-14 07:31:47 +0800

    init

---
+++ app/src/main/java/com/github/xch168/checkstyletest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.xch168.checkstyletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
