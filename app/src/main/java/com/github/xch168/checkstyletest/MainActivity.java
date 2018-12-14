93eca8ffacf833874c54d5c75cc19573cab41830
辉天神龙 <122565648@qq.com>
2018-12-14 08:07:12 +0800

    note

--- app/src/main/java/com/github/xch168/checkstyletest/MainActivity.java
+++ app/src/main/java/com/github/xch168/checkstyletest/MainActivity.java
@@ -1,14 +1,25 @@
 package com.github.xch168.checkstyletest;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    @SuppressWarnings({"memberName"})
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler.sendEmptyMessage(0);
     }
 }
