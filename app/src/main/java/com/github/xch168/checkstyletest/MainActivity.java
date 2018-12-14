e086f95626104734b5413448201295d1f4584cc9
徐灿辉 <xucanhui@4399inc.com>
2018-12-14 15:43:03 +0800

    pre-commit

--- app/src/main/java/com/github/xch168/checkstyletest/MainActivity.java
+++ app/src/main/java/com/github/xch168/checkstyletest/MainActivity.java
@@ -1,25 +1,17 @@
+
 package com.github.xch168.checkstyletest;
 
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressWarnings({"memberName"})
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        handler.sendEmptyMessage(0);
+
     }
 }
